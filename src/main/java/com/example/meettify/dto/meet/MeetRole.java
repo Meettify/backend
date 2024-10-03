b5409f67a373b724d0b58f2f44a10821db451890
whdudgms <108641428+whdudgms@users.noreply.github.com>
2024-10-03 11:46:53 +0900

    [Feat] 모임 회원 명단 보여주기

--- src/main/java/com/example/meettify/dto/meet/MeetRole.java
+++ src/main/java/com/example/meettify/dto/meet/MeetRole.java
@@ -8,6 +8,6 @@ package com.example.meettify.dto.meet;
  * */
 public enum MeetRole {
 
-    // 관리자, 멤버, 대기자, 추방자or비회원
-    ADMIN,MEMBER,WAITING,EXPEL
+    // 관리자, 멤버, 대기자, 비회원, 추방자
+    ADMIN,MEMBER,WAITING,OUTSIDER,EXPEL,
 }
