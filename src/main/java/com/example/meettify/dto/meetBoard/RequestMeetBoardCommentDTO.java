fb7c079efd3092e36a23d988a90b3b25a06c9dcb
whdudgms <108641428+whdudgms@users.noreply.github.com>
2024-10-06 00:32:34 +0900

    [Feat] 모임 게시판 대댓글 삭제 기능 구현

--- src/main/java/com/example/meettify/dto/meetBoard/RequestMeetBoardCommentDTO.java
+++ src/main/java/com/example/meettify/dto/meetBoard/RequestMeetBoardCommentDTO.java
@@ -1,16 +1,14 @@
 package com.example.meettify.dto.meetBoard;
 
-
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Null;
 import lombok.*;
 
 import java.time.LocalDateTime;
 
 /*
  *   worker  : 조영흔
- *   work    : 모임 Comment작성시 프론트가 보내주는 DTO
+ *   work    : 모임 Comment 작성시 프론트가 보내주는 DTO
  *   date    : 2024/10/03
  * */
 @ToString
@@ -20,23 +18,14 @@ import java.time.LocalDateTime;
 @Builder
 public class RequestMeetBoardCommentDTO {
 
-    @Schema(description = "모임 번호", example = "1")
-    @NotNull(message = "모임 번호는 필수 입니다.")
-    private Long meetId;
-
-    @Schema(description = "게시판 번호", example = "1")
-    @NotNull(message = "작성할 게시판 번호는 필수입니다.")
-    private Long meetBoardId;
-
     @Schema(description = "대댓글의 경우 부모를 보내줘야함", example = "1")
     private Long parentComment;
 
     @Schema(description = "댓글 내용", example = "정말 즐거운 댓글")
-    @NotNull(message = "댓글 내은 필수입니다.")
+    @NotNull(message = "댓글 내용은 필수입니다.")
     private String content;
 
     @Schema(description = "댓글 작성 시간")
     private LocalDateTime postDate;
-
 
 }
