0b20df56e0c7fe678bdb7d5e3118bd464386b28c
whdudgms <108641428+whdudgms@users.noreply.github.com>
2024-10-06 19:04:26 +0900

    [Feat] 모임 권한 enum에 Swagger 설명 추가

--- src/main/java/com/example/meettify/dto/meet/MeetRole.java
+++ src/main/java/com/example/meettify/dto/meet/MeetRole.java
@@ -1,6 +1,8 @@
 package com.example.meettify.dto.meet;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 /*
  *   worker  : 조영흔
  *   work    : 모임 관리자, 멤버, 대기자, 비회원, 제명자(강퇴당한 사람)
@@ -8,6 +10,14 @@ package com.example.meettify.dto.meet;
  * */
 public enum MeetRole {
 
-    // 관리자, 멤버, 대기자, 비회원, 추방자
-    ADMIN,MEMBER,WAITING,OUTSIDER,EXPEL,
+    @Schema(name = "관리자")
+    ADMIN,
+    @Schema(name = "멤버")
+    MEMBER,
+    @Schema(name = "대기 멤버")
+    WAITING,
+    @Schema(name = "비회원")
+    OUTSIDER,
+    @Schema(name = "추방자")
+    EXPEL,
 }
