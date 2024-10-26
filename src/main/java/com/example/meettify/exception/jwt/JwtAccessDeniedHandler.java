58ebf62216a811ad72e28782fd02151ff2a154db
YuYoHan <zxzz8014@naver.com>
2024-10-26 22:56:54 +0900

    [Modify] 토큰 예외처리 수정

--- src/main/java/com/example/meettify/exception/jwt/JwtAccessDeniedHandler.java
+++ src/main/java/com/example/meettify/exception/jwt/JwtAccessDeniedHandler.java
@@ -1,8 +1,11 @@
 package com.example.meettify.exception.jwt;
 
+import com.example.meettify.config.slack.RequestInfo;
+import com.example.meettify.config.slack.SlackUtil;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
@@ -10,17 +13,21 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 /*
  *   worker : 유요한
- *   work :
- *          JWT가 접근 권한이 없을 때 호출되는 역할입니다.
- *   date : 2024/09/19
+ *   work   : JWT가 접근 권한이 없을 때 호출되는 역할입니다.
+ *   date   : 2024/09/19
+ *   update : 2024/10/26
  * */
 @Component
+@RequiredArgsConstructor
 public class JwtAccessDeniedHandler implements AccessDeniedHandler {
+    private final SlackUtil slackUtil;
+
     @Override
     public void handle(HttpServletRequest request,
                        HttpServletResponse response,
                        AccessDeniedException accessDeniedException)
             throws IOException, ServletException {
-        response.sendError(HttpServletResponse.SC_FORBIDDEN, "접근 권한이 없습니다.");
+        slackUtil.sendAlert(accessDeniedException, new RequestInfo(request)); // Slack 알림 전송
+        response.sendError(HttpServletResponse.SC_FORBIDDEN, accessDeniedException.getMessage());
     }
 }
