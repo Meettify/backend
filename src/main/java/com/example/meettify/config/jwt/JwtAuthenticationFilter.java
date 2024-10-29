7ae649a5aabb9446008a9c3cf0c355ca2ac28aad
YuYoHan <zxzz8014@naver.com>
2024-10-29 18:34:40 +0900

    [Fix] 댓글 조회시 500번 에러 고치기

--- src/main/java/com/example/meettify/config/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/example/meettify/config/jwt/JwtAuthenticationFilter.java
@@ -46,6 +46,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         publicPaths.put("/api/v1/notice/**", List.of("GET"));
         publicPaths.put("/api/v1/items/**", List.of("GET"));
         publicPaths.put("/api/v1/community/**", List.of("GET"));
+        publicPaths.put("/api/v1/{communityId}/comment/**", List.of("GET"));
         publicPaths.put("/", List.of("GET"));
         publicPaths.put("/swagger-resources/**", List.of("GET"));
         publicPaths.put("/swagger-ui/**", List.of("GET"));
