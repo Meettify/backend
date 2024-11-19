a65a60d12673fa1cc72d4862152830dcc65c17df
YuYoHan <zxzz8014@naver.com>
2024-11-19 10:44:43 +0900

    [Remove] 사용하지 않는 클래스 삭제

--- src/main/java/com/example/meettify/repository/member/CustomMemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.meettify.repository.member;
-
-import com.example.meettify.entity.member.MemberEntity;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-
-public interface CustomMemberRepository {
-    Page<MemberEntity> findAll(Pageable pageable, String memberEmail);
-}
