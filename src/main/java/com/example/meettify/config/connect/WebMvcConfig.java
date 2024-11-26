5b9a863943284d07752779240b7d507412f3bad2
YuYoHan <zxzz8014@naver.com>
2024-11-26 23:43:18 +0900

    [Fix] SSE CORS 에러 고치기

--- src/main/java/com/example/meettify/config/connect/WebMvcConfig.java
+++ src/main/java/com/example/meettify/config/connect/WebMvcConfig.java
@@ -1,18 +1,18 @@
-package com.example.meettify.config.connect;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebMvcConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:5173", "do2867lf6anbu.cloudfront.net")
-                .allowedHeaders("Authorization", "Content-Type")
-                .allowCredentials(true)
-                .exposedHeaders("Cache-Control", "Content-Type", "X-Accel-Buffering","Authorization")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
-    }
-}
+//package com.example.meettify.config.connect;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebMvcConfig implements WebMvcConfigurer {
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOriginPatterns("http://localhost:5173", "do2867lf6anbu.cloudfront.net")
+//                .allowedHeaders("Authorization", "Content-Type")
+//                .allowCredentials(true)
+//                .exposedHeaders("Cache-Control", "Content-Type", "X-Accel-Buffering","Authorization")
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+//    }
+//}
