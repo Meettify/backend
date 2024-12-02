707fe50097e4dd929faa3fb07b7dd5057f3e7437
YuYoHan <zxzz8014@naver.com>
2024-12-02 19:02:47 +0900

    [Modify] 알림 리스트 조회기능 수정

--- src/main/java/com/example/meettify/repository/notification/NotificationRepository.java
+++ src/main/java/com/example/meettify/repository/notification/NotificationRepository.java
@@ -2,9 +2,10 @@ package com.example.meettify.repository.notification;
 
 import com.example.meettify.entity.notification.NotificationEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface NotificationRepository extends JpaRepository<NotificationEntity, Long> {
-    List<NotificationEntity> findAllByMemberMemberEmailOrderByRegTimeDesc(String email);
 }
