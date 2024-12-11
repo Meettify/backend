b1a81860a85d67970b1dfb97f7c2232973c8c8f8
YuYoHan <zxzz8014@naver.com>
2024-12-12 00:02:37 +0900

    [Modify] 결제 로직 수정

--- src/main/java/com/example/meettify/dto/pay/RequestTossPaymentConfirmDTO.java
+++ src/main/java/com/example/meettify/dto/pay/RequestTossPaymentConfirmDTO.java
@@ -31,18 +31,6 @@ public class RequestTossPaymentConfirmDTO {
     private String orderUid;
     @Schema(description = "주문 정보")
     private List<RequestOrderDTO> orders;
-    @Schema(description = "우편 번호")
-    private String memberAddr;
     @Schema(description = "주소")
-    private String memberAddrDetail;
-    @Schema(description = "상세 주소")
-    private String memberZipCode;
-
-    public static AddressDTO changeDTO(AddressEntity address) {
-        return AddressDTO.builder()
-                .memberAddr(address.getMemberAddr())
-                .memberAddrDetail(address.getMemberAddrDetail())
-                .memberZipCode(address.getMemberZipCode())
-                .build();
-    }
+    private AddressDTO address;
 }
