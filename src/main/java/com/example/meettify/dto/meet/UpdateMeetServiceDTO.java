1a80d0518be57aa0ca56f9ba02325232b2f75918
YuYoHan <zxzz8014@naver.com>
2025-05-19 22:22:13 +0900

    [Fix] 이미지 오타로 인하여 이미지 안나오는 에러 고침

--- src/main/java/com/example/meettify/dto/meet/UpdateMeetServiceDTO.java
+++ src/main/java/com/example/meettify/dto/meet/UpdateMeetServiceDTO.java
@@ -1,13 +1,7 @@
 package com.example.meettify.dto.meet;
 
 import com.example.meettify.dto.meet.category.Category;
-import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import lombok.*;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -32,9 +26,8 @@ public class UpdateMeetServiceDTO {
                 .meetDescription(meet.getMeetDescription())
                 .meetMaximum(meet.getMeetMaximum())
                 .meetLocation(meet.getMeetLocation())
-                .existingImageUrls(meet.getExigistingImages())
+                .existingImageUrls(meet.getExistingImages())
                 .category(meet.getCategory())
                 .build();
     }
-
 }
